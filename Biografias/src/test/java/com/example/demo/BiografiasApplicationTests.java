2c001e15d2b50edd5c81580fa295492c7cb98687
AlejandroPico <darimonium@gmail.com>
2025-05-24 23:09:35 +0200

    Iniciado proyecto Spring

---
+++ Biografias/src/test/java/com/example/demo/BiografiasApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiografiasApplicationTests {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BiografiasApplicationTests.class, args);
+	}
+
+}
